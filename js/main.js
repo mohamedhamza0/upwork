6ddc085d1f3248391dc10834bb10b835cf11a371
mohamedhamza0 <140764069+mohamedhamza0@users.noreply.github.com>
2023-09-08 00:43:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+alert('do you log in with google?');
+
+function scrl(){
+	var hddrr=document.getElementById('hdddrr');
+	var uppp=document.getElementById('up')
+		if(this.scrollY>=100){
+			hddrr.className="small";
+			uppp.className="small"
+		}
+		else{
+			hddrr.className="small";
+		}
+}
